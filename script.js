222dd9f82be3737d8ccbb6b3d80cfec48bc169e7
SanukaDewmina <161559862+SanukaDewmina@users.noreply.github.com>
2024-05-04 18:40:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const profileLogin =document.getElementById("profile"); 
+
+const userNameEL = document.getElementById("input-name");
+const userEmailEL = document.getElementById("input-email");
+const userPasswordEL = document.getElementById("input-password");
+const userNameCorectMsg = document.getElementById("profile01");
+
+
+function activeclasslist(){
+  profileLogin.classList.toggle("active");
+}
